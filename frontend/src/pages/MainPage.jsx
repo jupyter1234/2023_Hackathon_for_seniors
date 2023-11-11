b7e15d8eab094a061fe50b6b7dfbcf87e6341a29
wldnd2 <jiwoong000628@gmail.com>
2023-11-11 11:05:01 +0900

    식단표 및 강좌 시간표 추가

--- frontend/src/pages/MainPage.jsx
+++ frontend/src/pages/MainPage.jsx
@@ -1,7 +1,12 @@
+import MealSchedule from "../components/MealSchedule"
+import LectureSchedule from "../components/LectureSchedule"
+
 export default function MainPage() {
   return (
     <div>
       <h1>Main Page</h1>
+      <LectureSchedule/>
+      <MealSchedule/>
     </div>
   )
 }
