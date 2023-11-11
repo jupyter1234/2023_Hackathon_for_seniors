6abcbbe5ed408023c8befeafa0b027a33e7fc97a
wldnd2 <jiwoong000628@gmail.com>
2023-11-11 16:13:46 +0900

    프로그램 페이지 화면 구성

--- frontend/src/pages/PrgramPage.jsx
+++ frontend/src/pages/PrgramPage.jsx
@@ -1,7 +1,40 @@
+import ProgramCard from "../components/ProgramCard";
+import PROGRAMS from "../constants/LECTURE";
+import styled from "styled-components";
+
 export default function ProgramPage() { 
   return (
-    <div>
-      <h1>ProgramPage</h1>
-    </div>
+    <ProgramList>
+      <ProgramListTitle> 00복지관에서 <br />모집중인 프로그램 </ProgramListTitle>
+      <ProgramCardsContainer>
+        {PROGRAMS.map((program, idx) => (
+          <ProgramCard key={idx} program={program} />
+        ))}
+      </ProgramCardsContainer>
+  </ProgramList>
   );
 }
+
+const ProgramList = styled.div`
+  margin: 10px;
+  display: flex;
+  flex-direction: column;
+  width: 95%;
+`;
+
+const ProgramListTitle = styled.div`
+  position: relative;
+  font-size: 25px;
+  font-weight: bold;
+  margin: 5px;
+  border-bottom: 1px solid #000;
+`;
+
+const ProgramCardsContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  width: 100%;
+  height: 80vh;
+  overflow: scroll;
+`;
