a4ba952e39836b9da966cdc76ae6d492fea8bc43
bokyeong <wntjdals0412@gmail.com>
2023-11-11 16:00:29 +0900

    feat : 유저 로그인, 회원가입 again

--- backend/src/index.js
+++ backend/src/index.js
@@ -8,12 +8,13 @@ const cors = require('cors');
 
 // 라우터 추가
 const userRouter = require('./routes/UserRoute');
+const Welfare = require('./models/Welfare_fac');
 
 // express app 생성
 const app = express();
 
 app.use(cors());
-
+app.use(express.json());
 // DB 연결
 mongoose.connect(process.env.MONGO_URI, { dbName: 'Hackerton2023_Back' });
 
@@ -21,6 +22,30 @@ mongoose.connect(process.env.MONGO_URI, { dbName: 'Hackerton2023_Back' });
 mongoose.connection.on('connected', () => {
   console.log('Successfully connected to MongoDB');
 });
+//기관 위치 전부 가져오기
+app.get('/welfare', async function (req,res) {
+  try {
+    const welfares = await Welfare.find({},{_id:0,name:1,location:1})
+    res.send(
+      welfares
+      )
+    console.log("success")
+  } catch (error) {
+    console.log(error)
+  }
+})
+// Welfare.insertMany([
+//   {
+//     name : "달성군 북부노인복지관",
+//     location : {
+//       type: 'Point',
+      
+//       coordinates : [128.455040, 35.860659]
+//     }
+//   }
+// ]). then(
+//   console.log("더미데이터 생성 성공")
+// )
 
 app.get('/', (req, res) => {
   res.send('OK');
@@ -28,7 +53,10 @@ app.get('/', (req, res) => {
 
 // 라우터 이동
 app.use('/api/users', userRouter);
-//app.use('/user',require('./routes/user'))
+app.use('/user',require('./routes/user'));
+app.use('/board',require('./routes/board'));
+
+
 // express.req.body 안에 body-parser 기능들을 정의.
 app.use(bodyParser.urlencoded({ extended: false }));
 
