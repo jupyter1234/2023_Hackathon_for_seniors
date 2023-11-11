bbea35cc8e423442ac0f99b40e2cc56eb348ce43
wldnd2 <jiwoong000628@gmail.com>
2023-11-12 00:54:06 +0900

    커뮤니티 페이지 코드 수정

--- frontend/src/pages/Community/Community_Badook.jsx
+++ frontend/src/pages/Community/Community_Badook.jsx
@@ -1,92 +1,147 @@
 import { Link } from "react-router-dom";
-
-function Header(props){
-    return (
-      <div style={{margin: '0 auto', width: '100%', fontSize:'5vw'}}>
-        <h1>{props.children}</h1>
-      </div>
-    );
-}
-
-function ImgContext(props){
-    return(
-        <Link to='/board/badook' style={{textDecoration:'none'}}>
-            <div style={{display:'flex', alignItems:'center'}}>
-                <img className="imgSize" src="/img/park.jpg" style={{display:'inline-block', width:"100px",height:"100px",padding:"10px"}} alt="공원사진"/>
-                <div>
-                    <br/>
-                    <h2>{props.title}</h2>
-                    <article style={{fontSize:'18px'}}>
-                        {props.content}
-                    </article>
-                    <article style={{fontSize:'8px', textAlign:'right'}}>
-                        <br/>
-                        작성자: {props.writer}
-                    </article>
-                </div>
-            </div>
-        </Link>
-    );
-}
-
-function Context(props){
-    return(
-        <Link to='/board/badook' style={{textDecoration:'none'}}>
-            <div>
-                <br/>
-                    <h2>{props.title}</h2>
-                    <article style={{fontSize:'18px'}}>
-                        {props.content}
-                    </article>
-                    <article style={{fontSize:'8px', textAlign:'right'}}>
-                        <br/>
-                        작성자: {props.writer}
-                    </article>
-            </div>
-        </Link>
-    );
-}
+import styled from "styled-components";
 
 export default function Community_Board(props) {
-    //const params = props.location.state && props.location.state;
-    const buttonStyle = {
-        padding:'5px 15px',
-        borderRadius:'30px',
-        backgroundColor:'#000080',
-        color: '#ffffff',
-        border: 'none',
-        cursor: 'pointer',
-        fontSize: '20px',
-        fontWeight:'bold',
-    };  
-
-    return (
+  return (
+    <BoardContainer>
+      <Header>
+        <h1>함께 해요 게시판</h1>
+      </Header>
+      <CategorySection>
         <div>
-            <br/>
-            <Header>함께 해요 게시판</Header>
-            <br/>
-            <div style={{display:'flex',justifyContent:'space-between'}}>
-                <div>
-                    <p style={{fontWeight:'bold',display:'inline-block',textAlign:'left',fontSize:'20px',marginRight:'10px'}}>카테고리</p>
-                    <p style={{display:'inline-block',fontSize:'20px'}}>바둑</p>
-                </div>
-                <button style={buttonStyle}>글쓰기</button>
-            </div>
-            
-            <br/>
-            <hr/>
-
-            <div style={{overflowY: 'scroll',height:'400px',border:'1px solid #ccc', padding:"10px"}}>
-                <ImgContext title="매주 월요일마다 바둑..." content="쉼터공원에서 아침 9시에 바둑 같이..." writer="이세돌"/>
-                <hr/>
-                <Context title="매주 월요일마다 바둑..." content="쉼터공원에서 아침 9시에 바둑 같이..." writer="이세돌"/>
-                <hr/>
-                <ImgContext title="매주 목요일마다 바둑..." content="쉼터공원에서 아침 9시에 바둑 같이..." writer="이세돌"/>
-                <hr/>
-                <Context title="매주 월요일마다 바둑..." content="쉼터공원에서 아침 9시에 바둑 같이..." writer="이세돌"/>
-                <hr/>
-                <ImgContext title="매주 금요일마다 바둑..." content="쉼터공원에서 아침 9시에 바둑 같이..." writer="이세돌"/>
-            </div>
+          <p>카테고리</p>
+          <p>바둑</p>
         </div>
-    );
+        <button>글쓰기</button>
+      </CategorySection>
+      <hr />
+
+      <div style={{ overflowY: "scroll", height: "400px", border: "1px solid #ccc", padding: "10px" }}>
+        <BoardItem
+          to="/board/badook"
+          imgSrc="/img/park.jpg"
+          altText="공원사진"
+          title="매주 월요일마다 바둑..."
+          content="쉼터공원에서 아침 9시에 바둑 같이..."
+          writer="이세돌"
+        />
+        <hr />
+        <BoardItem
+          to="/board/badook"
+          imgSrc="/img/park.jpg"
+          altText="공원사진"
+          title="매주 월요일마다 바둑..."
+          content="쉼터공원에서 아침 9시에 바둑 같이..."
+          writer="이세돌"
+        />
+        <hr />
+        <BoardItem
+          to="/board/badook"
+          imgSrc="/img/park.jpg"
+          altText="공원사진"
+          title="매주 월요일마다 바둑..."
+          content="쉼터공원에서 아침 9시에 바둑 같이..."
+          writer="이세돌"
+        />
+        <hr />
+        <BoardItem
+          to="/board/badook"
+          imgSrc="/img/park.jpg"
+          altText="공원사진"
+          title="매주 월요일마다 바둑..."
+          content="쉼터공원에서 아침 9시에 바둑 같이..."
+          writer="이세돌"
+        />
+        <hr />
+        {/* Add more BoardItems as needed */}
+      </div>
+    </BoardContainer>
+  );
 }
+
+function BoardItem({ to, imgSrc, altText, title, content, writer }) {
+  return (
+    <BoardItemContainer to={to}>
+      <BoardItemImage className="imgSize" src={imgSrc} alt={altText} />
+      <BoardItemContent>
+        <BoardItemTitle>{title}</BoardItemTitle>
+        <BoardItemText>{content}</BoardItemText>
+        <BoardItemDetails>작성자: {writer}</BoardItemDetails>
+      </BoardItemContent>
+    </BoardItemContainer>
+  );
+}
+
+const Header = styled.div`
+  margin: 0 auto;
+  width: 100%;
+  font-size: 5vw;
+
+  h1 {
+    margin: 0;
+  }
+`;
+
+const BoardContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-top: 20px;
+`;
+
+const CategorySection = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  p {
+    font-weight: bold;
+    font-size: 20px;
+    margin-right: 10px;
+  }
+
+  button {
+    padding: 5px 15px;
+    border-radius: 30px;
+    background-color: #000080;
+    color: #ffffff;
+    border: none;
+    cursor: pointer;
+    font-size: 20px;
+    font-weight: bold;
+  }
+`;
+
+const BoardItemContainer = styled(Link)`
+  display: flex;
+  align-items: center;
+  text-decoration: none;
+  color: inherit;
+`;
+
+const BoardItemImage = styled.img`
+  display: inline-block;
+  width: 100px;
+  height: 100px;
+  padding: 10px;
+`;
+
+const BoardItemContent = styled.div`
+  margin-left: 10px;
+  max-width: 70%;
+`;
+
+const BoardItemTitle = styled.h2`
+  font-size: 20px;
+  font-weight: bold;
+  margin: 0;
+`;
+
+const BoardItemText = styled.article`
+  font-size: 18px;
+`;
+
+const BoardItemDetails = styled.div`
+  font-size: 8px;
+  text-align: right;
+  margin-top: 10px;
+`;
