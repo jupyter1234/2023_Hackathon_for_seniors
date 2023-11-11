262429a156f4bd75fd6dade7f12cc5fe2e6b2faf
wldnd2 <jiwoong000628@gmail.com>
2023-11-11 14:16:29 +0900

    메인 화면 구성 완료

---
+++ frontend/src/constants/HOME.js
@@ -0,0 +1,20 @@
+const HOMEITEMS = [
+  {
+    title: '함께해요 게시판',
+    url_path: '/community',
+  },
+  {
+    title: '식단표',
+    url_path: '/meals',
+  },
+  {
+    title: '강좌 시간표',
+    url_path: '/lecture',
+  },
+  {
+    title: '내가 쓴 글',
+    url_path: '/myboard',
+  },
+];
+
+export default HOMEITEMS;
--- frontend/src/pages/MainPage.jsx
+++ frontend/src/pages/MainPage.jsx
@@ -1,16 +1,25 @@
 import ProgramCard from '../components/ProgramCard';
-import PROGRAMS from '../constants/TEMPDATA';
+import HomeItemCard from '../components/HomeItemCard';
+import PROGRAMS from '../constants/LECTURE';
+import HOMEITEMS from '../constants/HOME';
 import styled from 'styled-components';
 
 export default function MainPage() {
   return (
     <MainPageContainer>
-      <h1>Main Page</h1>
-      <ProgramCardsContainer>
-        {PROGRAMS.map((program, idx) => (
-          <ProgramCard key={idx} program={program} />
+      <HomeCardsContainer>
+        {HOMEITEMS.map((item, idx) => (
+          <HomeItemCard key={idx} item={item} />
         ))}
-      </ProgramCardsContainer>
+      </HomeCardsContainer>
+      <ProgramList>
+        <ProgramListTitle> 모집중인 프로그램 </ProgramListTitle>
+        <ProgramCardsContainer>
+          {PROGRAMS.map((program, idx) => (
+            <ProgramCard key={idx} program={program} />
+          ))}
+        </ProgramCardsContainer>
+      </ProgramList>
     </MainPageContainer>
   );
 }
@@ -24,8 +33,33 @@ const MainPageContainer = styled.div`
   overflow: scroll;
 `;
 
+const HomeCardsContainer = styled.div`
+  width: 100%;
+  height: 400px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+const ProgramList = styled.div`
+  margin: 10px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+`;
+
+const ProgramListTitle = styled.div`
+  position: relative;
+  font-size: 25px;
+  font-weight: bold;
+  margin: 5px;
+  border-bottom: 1px solid #000;
+`;
+
 const ProgramCardsContainer = styled.div`
-  border: 1px solid #ddd;
   width: 100%;
   height: 270px;
   display: flex;
