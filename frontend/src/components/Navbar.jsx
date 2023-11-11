6fd465f5dfb74894d0f6dccde27edf72e159429b
wldnd2 <jiwoong000628@gmail.com>
2023-11-11 09:42:43 +0900

    네비게이션 바 구현

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,39 @@
+import { useRef } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
+import { Link } from "react-router-dom";
+import "./Navbar.css";
+
+function Navbar() {
+	const navRef = useRef();
+
+	const showNavbar = () => {
+		navRef.current.classList.toggle(
+			"responsive_nav"
+		);
+	};
+
+	return (
+		<header>
+			<h3>LOGO</h3>
+			<nav ref={navRef}>
+				<Link to="/" onClick={showNavbar}>홈</Link>
+				<Link to="/map" onClick={showNavbar}>지도</Link>
+				<Link to="/register" onClick={showNavbar}>등록</Link>
+				<Link to="/community" onClick={showNavbar}>커뮤니티</Link>
+				<Link to="/login" onClick={showNavbar}>로그인</Link>
+				<button
+					className="nav-btn nav-close-btn"
+					onClick={showNavbar}>
+					<FaTimes />
+				</button>
+			</nav>
+			<button
+				className="nav-btn"
+				onClick={showNavbar}>
+				<FaBars />
+			</button>
+		</header>
+	);
+}
+
+export default Navbar;
