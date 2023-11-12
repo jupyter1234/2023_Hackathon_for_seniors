b2da962017c200f0322c0baea4af66330ec0e08c
wldnd2 <jiwoong000628@gmail.com>
2023-11-12 10:41:57 +0900

    글쓰기 ui 수정

--- frontend/src/pages/Community_Write.jsx
+++ frontend/src/pages/Community_Write.jsx
@@ -1,46 +1,108 @@
 import { Link } from "react-router-dom";
-
-function Header(props){
-    return (
-      <div style={{margin: '0 auto', width: '100%', fontSize:'5vw'}}>
-        <h1>{props.children}</h1>
-      </div>
-    );
-}
+import styled from 'styled-components';
+import { useState } from "react";
 
 export default function Community_Write() {
-    const buttonStyle = {
-        padding:'5px 15px',
-        borderRadius:'30px',
-        backgroundColor:'#000080',
-        color: '#ffffff',
-        border: 'none',
-        cursor: 'pointer',
-        fontSize: '20px',
-        fontWeight:'bold',
-    };  
+  const [title, setTitle] = useState('');
+  const [content, setContent] = useState('');
 
-    return(
-        <div>
-            <br/>
-                <Header>함께 해요 게시판</Header>
-            <br/>
-                <div style={{color:'gray',fontWeight:'bold', display:'inline-block'}}>
-                    제목을 입력하세요
-                </div>
-            <hr/>
-            <br/>
-            <table style={{height:'500px'}}>
-                <td>
-                    <div style={{color:'gray',fontWeight:'bold', display:'inline-block'}}>
-                            내용을 입력하세요
-                    </div>
-                </td>
-            </table>
-            <br/>
-            <Link to="/community/badook">
-                <button style={buttonStyle}>제출</button>
-            </Link> 
-        </div>
-    );
+  const handleTitleChange = (event) => {
+    setTitle(event.target.value);
+  };
+
+  const handleContentChange = (event) => {
+    setContent(event.target.value);
+  };
+
+  const handleSubmit = () => {
+    console.log("Title:", title);
+    console.log("Content:", content);
+  };
+
+  return (
+    <AllContainer>
+      <br />
+      <HeaderWrapper>
+        <h1>함께 해요 게시판</h1>
+      </HeaderWrapper>
+      <br />
+      <ContentWrapper>
+        <input placeholder="제목을 입력하세요" type="text" value={title} onChange={handleTitleChange} />
+      </ContentWrapper>
+      <hr />
+      <StyledTable>
+        <td>
+          <ContentWrapper>
+            <textarea placeholder="내용을 입력하세요" value={content} onChange={handleContentChange} />
+          </ContentWrapper>
+        </td>
+      </StyledTable>
+      <br />
+      <Link to="/community/badook">
+        <SubmitButton onClick={handleSubmit}>제출</SubmitButton>
+      </Link>
+    </AllContainer>
+  );
 }
+
+const AllContainer = styled.div`
+    width: 100%;
+    height: 80vh;
+    margin: 0 auto;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    padding: 20px;
+`;
+
+const HeaderWrapper = styled.div`
+  margin: 0 auto;
+  width: 100%;
+  font-size: 5vw;
+`;
+
+const ContentWrapper = styled.div`
+    color: gray;
+    font-weight: bold;
+    display: inline-block;
+    margin: 10px;
+
+    input {
+        border-radius: 8px;
+        border: 1px solid #ddd;
+        padding: 10px;
+        font-size: 20px;
+        margin-top: 20px;
+        width: 100%;
+
+        ::placeholder {
+            color: gray;
+            font-weight: bold;
+        }
+    }
+
+    textarea {
+        width: 300px;
+        height: 300px;
+        font-size: 20px;
+        border: none;
+        border: 1px solid #ddd;
+    }
+`;
+
+const SubmitButton = styled.button`
+    width: 200px;
+    padding: 5px 10px;
+    border-radius: 30px;
+    background-color: #000080;
+    color: #ffffff;
+    border: none;
+    cursor: pointer;
+    font-size: 30px;
+    font-weight: bold;
+`;
+
+const StyledTable = styled.table`
+    height: 500px;
+`;
